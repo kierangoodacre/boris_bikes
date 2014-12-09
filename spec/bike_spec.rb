a08c8d921608bea280f80f3f83b5f34de6142a54
Kieran Goodacre <kierangoodacre@Kierans-MacBook-Pro.local>
2014-12-09 14:17:22 +0000

    started work on rspec

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,1 +1,8 @@
-iauiasud
+require 'bike'
+
+describe Bike do
+	it "should not be broken after we create " do
+	the_bike = Bike.new
+	expect(the_bike).not_to be_broken # expect() object under test as an argument and returns special object, that has different methods, that make it pass or fail, depending on the arguments 
+ end
+end
