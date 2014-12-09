64b7ba01f6e278aa1bbe69795f5f41390af536be
Kieran Goodacre <kierangoodacre@Kierans-MacBook-Pro.local>
2014-12-09 15:44:49 +0000

    added tests

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,7 +2,12 @@ require 'bike'
 
 describe Bike do
 	it "should not be broken after we create " do
-	the_bike = Bike.new
-	expect(the_bike).not_to be_broken # expect() object under test as an argument and returns special object, that has different methods, that make it pass or fail, depending on the arguments 
- end
+		the_bike = Bike.new
+		expect(the_bike).not_to be_broken # expect() object under test as an argument and returns special object, that has different methods, that make it pass or fail, depending on the arguments  
+ 	end
+ 	it "should be able to break" do
+ 		bike = Bike.new
+ 		bike.break!
+ 		expect(bike).to be_broken
+ 	end
 end
