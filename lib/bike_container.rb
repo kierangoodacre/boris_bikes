da6164d8b38daff62b181df287ef48244594ae90
Kieran Goodacre <kierangoodacre@Kierans-MacBook-Pro.local>
2014-12-10 19:15:13 +0000

    added bike container module

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -1,32 +1,37 @@
 module BikeContainer
 
- 	DEFAULT_CAPACITY = 10 
+  DEFAULT_CAPACITY = 10
 
-	def initialize(options = {})
-		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-		@bikes = []
-	end
+  def bikes
+    @bikes ||= []
+  end
 
-	def bike_count
-		@bikes.count
-	end
+  def capacity
+    @capacity ||= DEFAULT_CAPACITY
+  end
 
-	def dock(bike)
-		raise "Station is full" if full?
-		@bikes << bike
-	end
+  def capacity=(value)
+    @capacity = value
+  end
 
-	def release(bike)
-		@bikes.delete(bike)
-	end
+  def bike_count
+    bikes.count
+  end
 
-	def full?
-		bike_count == @capacity
-	end
+  def dock(bike)
+    raise "Station is full" if full? #should be Holder?
+    bikes << bike
+  end
 
-	def available_bikes
-		@bikes.reject {|bike| bike.broken?}
-	end
+  def release(bike)
+    bikes.delete(bike)
+  end
 
+  def full?
+    bike_count == capacity
+  end
 
+  def available_bikes
+    bikes.reject {|bike| bike.broken? }
+  end
 end
